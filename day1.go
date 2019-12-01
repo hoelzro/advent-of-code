cae55eb52fc14e1badd00a3fa6e15616d47106a9
Rob Hoelz <rob@hoelz.ro>
2019-12-01 07:35:16 -0600

    Update day 1 for part 2

--- day1.go
+++ day1.go
@@ -12,7 +12,7 @@ func main() {
 		return
 	}
 
-	fuel := 0
+	totalFuel := 0
 
 	for _, arg := range os.Args[1:] {
 		mass, err := strconv.Atoi(arg)
@@ -22,8 +22,17 @@ func main() {
 			os.Exit(1)
 		}
 
-		fuel += (mass / 3) - 2
+		fuel := (mass / 3) - 2
+
+		fuelForModule := 0
+
+		for fuel > 0 {
+			fuelForModule += fuel
+			fuel = (fuel / 3) - 2
+		}
+
+		totalFuel += fuelForModule
 	}
 
-	fmt.Printf("fuel required: %d\n", fuel)
+	fmt.Printf("total fuel required: %d\n", totalFuel)
 }
