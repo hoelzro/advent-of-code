981eca1f73a7cca7ed1e23057306d9629493cc4e
Rob Hoelz <rob@hoelz.ro>
2019-12-01 07:33:11 -0600

    Add code for day 1 part 1

---
+++ day1.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "usage: %s [masses...]\n", os.Args[0])
+		return
+	}
+
+	fuel := 0
+
+	for _, arg := range os.Args[1:] {
+		mass, err := strconv.Atoi(arg)
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "couldn't parse %s as an integer: %v\n", arg, err)
+			os.Exit(1)
+		}
+
+		fuel += (mass / 3) - 2
+	}
+
+	fmt.Printf("fuel required: %d\n", fuel)
+}
